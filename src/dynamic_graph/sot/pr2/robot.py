75a55a5f290e65a718778b247bc465a535efb926
bcoudrin <bcoudrin@laas.fr>
2013-10-02 17:46:00 +0200

    Make python Pr2 derive from AbstractRobot and use RosSotRobotModel as Dynamic

--- src/dynamic_graph/sot/pr2/robot.py
+++ src/dynamic_graph/sot/pr2/robot.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Copyright 2013, Benjamin Coudrin, LIRMM, CNRS
 # Copyright 2011, Florent Lamiraux, Thomas Moulard, JRL, CNRS/AIST
 #
 # This file is part of dynamic-graph.
@@ -14,31 +15,28 @@
 # received a copy of the GNU Lesser General Public License along with
 # dynamic-graph. If not, see <http://www.gnu.org/licenses/>.
 
-from dynamic_graph.sot.dynamics.humanoid_robot import AbstractHumanoidRobot
-from dynamic_graph.ros import RosRobotModel
+from dynamic_graph.sot.dynamics.abstract_robot import AbstractRobot
+from dynamic_graph.ros.ros_sot_robot_model import RosSotRobotModel
 
-class Pr2(AbstractHumanoidRobot):
+class Pr2(AbstractRobot):
     """
     This class instanciates a Pr2 robot.
     """
 
-    OperationalPoints = []
+    OperationalPoints = ['right-wrist','left-wrist','waist','gaze']
 
     tracedSignals = {
-        'dynamic': ["com", "zmp", "position", "velocity", "acceleration"],
-        'device': ['zmp', 'control', 'state']
+        'dynamic': ["com", "position", "velocity", "acceleration"],
+        'device': ['control', 'state']
         }
 
     def __init__(self, name, device = None, tracer = None):
-        AbstractHumanoidRobot.__init__ (self, name, tracer)
+        AbstractRobot.__init__ (self, name, tracer)
         self.device = device
-        self.dynamic = RosRobotModel("{0}_dynamic".format(name))
+        self.dynamic = RosSotRobotModel("{0}_dynamic".format(name))
         self.dynamic.loadFromParameterServer()
-        #self.dynamic.loadUrdf("package://pr2_mechanism_model/pr2.urdf")
-
         self.dimension = self.dynamic.getDimension()
         self.halfSitting = (0.,) * self.dimension
-        print(self.halfSitting)
         self.initializeRobot()
 
 __all__ = ["Pr2"]
