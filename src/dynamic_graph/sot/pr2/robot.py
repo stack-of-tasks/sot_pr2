7c9b93736ac4ffbc783662d95ec6d159ac125b55
Benjamin Coudrin <benjamin.coudrin@laas.fr>
2013-11-05 15:27:08 +0100

    Replace RosSotRobotModel with the modified RosRobotModel from dynamic_graph_bridge

--- src/dynamic_graph/sot/pr2/robot.py
+++ src/dynamic_graph/sot/pr2/robot.py
@@ -17,7 +17,7 @@
 
 #from dynamic_graph.sot.dynamics.abstract_robot import AbstractRobot
 from dynamic_graph.sot.dynamics.humanoid_robot import AbstractHumanoidRobot
-from dynamic_graph.ros.ros_sot_robot_model import RosSotRobotModel
+from dynamic_graph.ros.robot_model import RosRobotModel
 
 class Pr2(AbstractHumanoidRobot):
     """
@@ -44,7 +44,7 @@ class Pr2(AbstractHumanoidRobot):
     def __init__(self, name, device = None, tracer = None):
         AbstractHumanoidRobot.__init__ (self, name, tracer)
         self.device = device
-        self.dynamic = RosSotRobotModel("{0}_dynamic".format(name))
+        self.dynamic = RosRobotModel("{0}_dynamic".format(name))
         self.specifySpecialLinks()
         self.dynamic.loadFromParameterServer()
         self.dimension = self.dynamic.getDimension()
